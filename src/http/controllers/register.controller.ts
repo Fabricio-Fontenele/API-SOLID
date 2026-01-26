45f9f370cb0eb536834ad3c80bd1ad3ca005e2d5
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2026-01-26 11:58:29 -0300

    feat: implement service factories for authentication and registration

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,6 +1,5 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UserAlreadyExistsError } from '@/services/errors/user-already-exists-error'
-import { RegisterService } from '@/services/register.service'
+import { makeRegisterService } from '@/services/factories/make-register-service'
 
 import { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
@@ -15,8 +14,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    const usersRepository = new PrismaUsersRepository()
-    const registerService = new RegisterService(usersRepository)
+    const registerService = makeRegisterService()
 
     await registerService.execute({
       name,
