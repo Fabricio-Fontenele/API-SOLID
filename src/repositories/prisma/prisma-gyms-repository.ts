e167eac13bee7512aef7436f16e406001093e1e1
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2026-02-01 19:35:18 -0300

    feat: implement PrismaGymsRepository with methods for gym management

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,56 @@
+import { Gym, Prisma } from '@prisma/client'
+import { findManyNearbyParams, GymsRepository } from '../gyms-repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaGymsRepository implements GymsRepository {
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    })
+    return gym
+  }
+
+  async findManyNearby({ latitude, longitude }: findManyNearbyParams) {
+    const radiusKm = 10
+
+    const gyms = await prisma.$queryRaw<Gym[]>`
+    SELECT *
+    FROM gyms
+    WHERE (
+      6371 * acos(
+        LEAST(1, GREATEST(-1,
+          cos(radians(${latitude})) * cos(radians(latitude)) *
+          cos(radians(longitude) - radians(${longitude})) +
+          sin(radians(${latitude})) * sin(radians(latitude))
+        ))
+      )
+    ) <= ${radiusKm}
+  `
+
+    return gyms
+  }
+
+  async serachMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return gyms
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data,
+    })
+
+    return gym
+  }
+}
