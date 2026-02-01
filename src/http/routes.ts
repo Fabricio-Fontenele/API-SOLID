60a419632f17bbc0a2cef18e6e1ed06c221de089
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2026-02-01 20:54:10 -0300

    feat: restructure authentication and registration controllers, add profile endpoint

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,12 @@
-import { authenticate } from './controllers/authenticate.controller'
-import { register } from './controllers/register.controller'
+import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
+import { register } from './controllers/register'
 import { FastifyInstance } from 'fastify'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
-
   app.post('/sessions', authenticate)
+
+  // Authentication required below
+  app.get('/me', profile)
 }
