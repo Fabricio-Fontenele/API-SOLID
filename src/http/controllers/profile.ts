8d11b16ea8e319f2be4353fcaefc2c48e23bdf5c
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2026-02-02 22:43:06 -0300

    feat: add JWT authentication with fastify-jwt and update environment configuration

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,5 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  await request.jwtVerify()
+
   reply.status(200).send()
 }
