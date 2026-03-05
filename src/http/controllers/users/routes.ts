c3deccdddc81ca808950d83a24fdc32d5fa20282
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2026-03-04 22:40:34 -0300

    feat: restructure user authentication and profile management with new routes and tests

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,8 +1,8 @@
-import { authenticate } from './controllers/authenticate'
-import { profile } from './controllers/profile'
-import { register } from './controllers/register'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
-import { verifyJWT } from './middlewares/verify-jwt'
+import { register } from './register'
+import { authenticate } from './authenticate'
+import { profile } from './profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
