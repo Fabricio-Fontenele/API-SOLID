a66a2ee852c4242ffca37b2519f30e1230b666dc
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2026-03-07 21:41:35 -0300

    feat: implement refresh token endpoint and add e2e tests for token refresh functionality

---
+++ src/http/controllers/users/refresh.ts
@@ -0,0 +1,41 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import z from 'zod'
+
+export async function refresh(request: FastifyRequest, reply: FastifyReply) {
+  await request.jwtVerify({ onlyCookie: true })
+
+  const refreshBodySchema = z.object({
+    email: z.email(),
+    password: z.string().min(6),
+  })
+
+  const token = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: request.user.sub,
+      },
+    },
+  )
+
+  const refreshToken = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: request.user.sub,
+        expiresIn: '7d',
+      },
+    },
+  )
+  return reply
+    .setCookie('refreshToken', refreshToken, {
+      path: '/',
+      secure: true,
+      sameSite: true,
+      httpOnly: true,
+    })
+    .status(200)
+    .send({
+      token,
+    })
+}
